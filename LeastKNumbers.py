7ac7a396ad5e5d2d8c899c941c35d9a47d5acd68
David Gao <582435572@qq.com>
2021-09-07 15:01:04 +0800

    Update LeastKNumbers.py
    only work for inputs with diff occurrence

--- LeastKNumbers.py
+++ LeastKNumbers.py
@@ -2,19 +2,20 @@
 class Solution:
     def GetLeastNumbers_Solution(self, tinput, k):
         # write code here
-        answer = [-1] * len(tinput)
         left, right = 0, len(tinput) - 1
-        number = 0
-
-        while (number < k):
-            piviot = self.partition(tinput, left, right)
-            if piviot <= k:
-                answer[piviot] = tinput[-1]
-                number += 1
-            else:
-                right = piviot - 1
-
-        return answer[k:]
+        answer = []
+        if len(tinput) == 0 or k == 0:
+            return []
+        piviot, tinput = self.partition(tinput, left, right)
+        if piviot > k - 1:
+            answer.append(self.GetLeastNumbers_Solution(tinput[:piviot], k))
+        if piviot < k - 1:
+            answer.append(tinput[piviot:])
+            answer.append(self.GetLeastNumbers_Solution(tinput[:piviot], k - piviot))
+        if piviot == k - 1:
+            answer.append(tinput[:piviot + 1])  # include pivot
+        answer = [item for sublist in answer for item in sublist]  # flatten the list
+        return answer
 
     def partition(self, tinput, left, right):
         piviot = right
@@ -26,16 +27,23 @@ class Solution:
                 index += 1
             else:
                 result.append(tinput[i])
-        return index
-
-    def swap(self, i, j, arr):
-        swp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = swp
+        return index, result
 
 
-test1 = [4, 5, 1, 6, 2, 7, 3, 8]
-n = 4
 s = Solution()
-result = s.GetLeastNumbers_Solution(test1, n)
-print(result)
+# n1 = 4
+# test1 = [4, 5, 1, 6, 2, 7, 3, 8]
+# result1 = s.GetLeastNumbers_Solution(test1, n1)
+# print(result1)
+# test2 = [1]
+# n2 = 0
+# result2 = s.GetLeastNumbers_Solution(test2, n2)
+# print(result2)
+test3 = [0, 1, 2, 1, 2]
+n3 = 3
+result3 = s.GetLeastNumbers_Solution(test3, n3)
+print(result3)
+# test4 = [234,233,233,233,233,233,233,233,233,233,233,233,233,233,233,233,233,233,233,233]
+# n4 = 10
+# result4 = s.GetLeastNumbers_Solution(test4, n4)
+# print(result4)
