3185c4aeebac2e44b1a6338a6212a2771f389a0b
David Gao <582435572@qq.com>
2021-09-18 12:24:25 +0800

    Update shortestBridge.py

--- shortestBridge.py
+++ shortestBridge.py
@@ -39,24 +39,28 @@ from typing import List
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
+"""
+多起點，多終點
+起點：用DFS找
+終點：用BFS找
+"""
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         def dfs(grid: List[List[int]], x: int, y: int, queue: List):
             # 如果没有visit，标记为visit，接着想四周阔
             if 0 <= x <= len(grid) - 1 and 0 <= y <= len(grid[x]) - 1 and grid[x][y] != 2:
                 grid[x][y] = 2  # 2: visited
-                # add visited island to queue
-                queue.append([x, y])
+                queue.append([x, y]) # add visited island to queue
 
-                # 向四个方向延伸
+                # 向四个方向延伸找终点
                 dfs(grid, x - 1, y, queue)  # up
                 dfs(grid, x, y - 1, queue)  # left
                 dfs(grid, x + 1, y, queue)  # down
                 dfs(grid, x, y + 1, queue)  # right
 
         # 1. 找到这两座岛(dfs)
-        # 2. 选择一座，将它不断向外延伸一圈(bfs)，直到到达了另一座岛
-        # 深度优先搜索grid 中的 1，再从source中的所有位置开始进行广度优先搜索
+        # 2. 选择一座，将它不断向外扩展一圈(bfs)已知领域，直到发现了另一座岛
+        # 深度优先搜索grid 中的 1，再从source中的所有位置开始进行深度优先搜索
         # 在向外延伸时，用广度优先搜索
         queue = []
         # 找到其中一个小岛
