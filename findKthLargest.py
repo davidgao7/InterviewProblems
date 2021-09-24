faef6846743c617422121b133118a5ade32d52a7
David Gao <582435572@qq.com>
2021-09-24 20:14:09 +0800

    Update findKthLargest.py

--- findKthLargest.py
+++ findKthLargest.py
@@ -4,38 +4,28 @@ class Solution:
     def findKth(self, a, n, K):
         # write code here
         # quick select
-        left, right = 0, n - 1
-        pivoit, a = self.partition(a, left, right)
-        while pivoit != K - 1:
-            if pivoit > K - 1:
-                pivoit, a = self.partition(a, left, pivoit)
-            elif pivoit < K - 1:
-                pivoit, a = self.partition(a, pivoit + 1, right)
 
-        return a[pivoit]
+        # choosinig pivoit
+        pivoit = n - 1
 
-    def partition(self, a, left, right):
-        pivoit = right
-        index_pivoit = -1
-        ref = []
+        # divide
+        left = [x for x in a if x > a[pivoit]] # remember to use a[pivoit] instead of pivoit which is a index not actual number!
+        mid = [x for x in a if x == a[pivoit]]
+        right = [x for x in a if x < a[pivoit]] # find the largest first 从大到小
 
-        for i in range(left, right):
-            if a[i] < a[pivoit]:
-                ref.append(a[i])
-                index_pivoit += 1
-        for i in range(left, right):
-            if a[i] == a[pivoit]:
-                ref.append(a[i])
-                index_pivoit += 1
-        for i in range(left, right):
-            if a[i] > a[pivoit]:
-                ref.append(a[i])
-
-        index_pivoit = index_pivoit + left
-        return index_pivoit, ref
+        l, m, r = len(left), len(mid), len(right)
+        # if k in the left arr
+        if K <= l:
+            return self.findKth(left, l, K) # l only stand for length of left, not effect overall algorithm
+        # if k on the right side
+        elif K > l + m:
+            return self.findKth(right, r, K - (l + m)) # r only stand for length of right, not effect overall algorithm
+        # otherwise it means the mid # is the kth largest
+        else:
+            return mid[0]
 
 
 s = Solution()
-a, n, K = [1332802,1177178,1514891,871248,753214,123866,1615405,328656,1540395,968891,1884022,252932,1034406,1455178,821713,486232,860175,1896237,852300,566715,1285209,1845742,883142,259266,520911,1844960,218188,1528217,332380,261485,1111670,16920,1249664,1199799,1959818,1546744,1904944,51047,1176397,190970,48715,349690,673887,1648782,1010556,1165786,937247,986578,798663],49,24
+a, n, K = [10, 10, 9, 9, 8, 7, 5, 6, 4, 3, 4, 2], 12, 3
 e = s.findKth(a, n, K)
 print(e)
