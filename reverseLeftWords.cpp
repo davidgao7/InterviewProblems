f012970fe0c682f7b014741f95b44d93fb92d8e4
David Gao <582435572@qq.com>
2021-10-08 15:18:37 +0800

    Update reverseLeftWords.cpp
    执行用时：
    0 ms
    , 在所有 C++ 提交中击败了
    100.00%
    的用户
    内存消耗：
    7.5 MB
    , 在所有 C++ 提交中击败了
    47.09%
    的用户

--- reverseLeftWords.cpp
+++ reverseLeftWords.cpp
@@ -7,17 +7,25 @@ using namespace std; // for cout, endl, string class
 class Solution {
 public:
     string reverseLeftWords(string s, int n) {
-        return s;
+        string answer = "";
+        for(int i = n; i < s.length(); i++){
+            answer+=s[i];
+        }
+        for (int i = 0; i < n; i++) {
+            answer+=s[i];
+        }
+        return answer;
     }
 };
 
 int main(){
     Solution *solution = new Solution();
     string s = "abcdefg";
+    printf("origin: %s\n", s.c_str());
     int k = 2;
     string answer = solution->reverseLeftWords(s, k);
     const char* string1 = answer.c_str();
-    printf("%s", string1); // stdio.h, %s need to be char* not string
+    printf("%s", string1); // NOTE: stdio.h, %s need to be char* not string
     // The pointer is such that the range [c_str(); c_str() + size()] is valid and the values
     // in it correspond to the values stored in the string with an additional null character
     // after the last position.
