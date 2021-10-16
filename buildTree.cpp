e0c176de7080611d68cb3fe3e392b9aa6f080cb7
David Gao <582435572@qq.com>
2021-10-16 15:21:52 +0800

    setup

--- buildTree.cpp
+++ buildTree.cpp
@@ -1,3 +1,70 @@
+//输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
+//
+// 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+//
+//
+//
+// 示例 1:
+//
+//
+//Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+//Output: [3,9,20,null,null,15,7]
+//
+//
+// 示例 2:
+//
+//
+//Input: preorder = [-1], inorder = [-1]
+//Output: [-1]
+//
+//
+//
+//
+// 限制：
+//
+// 0 <= 节点个数 <= 5000
+//
+//
+//
+// 注意：本题与主站 105 题重复：https://leetcode-cn.com/problems/construct-binary-tree-from-
+//preorder-and-inorder-traversal/
+// Related Topics 树 数组 哈希表 分治 二叉树 👍 585 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+using namespace std;
+#include "vector"
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
         // preorder: N L R [中 左 右]
         // inorder: L N R [左 中 右]
         // postorder: L R N [左 右 中]
+
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+int main(){
+    printf("hello world\n");
+    return 0;
+};
