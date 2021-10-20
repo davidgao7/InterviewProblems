3e89a8ad39523d31b6c3e04bb1938dae31062a9b
David Gao <582435572@qq.com>
2021-10-20 17:34:08 +0800

    Update separateWords.cpp

--- separateWords.cpp
+++ separateWords.cpp
@@ -12,20 +12,31 @@ int main() {
 
     vector<string> words;
     stringstream ss;
-    ss<<"some random words";
+    ss << "some random words";
     string word;
 
     while (getline(ss, word, ' ')) {
         words.push_back(word);
     }
 
-    cout<<"using regular for loop"<<endl;
-    for (int i = 0; i < words.size(); i++){
+    cout << "using regular for loop" << endl;
+    for (int i = 0; i < words.size(); i++) {
         cout << words[i] << endl;
     }
 
-    cout<<"using auto for loop"<<endl;
+    cout << "using auto for loop" << endl;
     for (auto x: words) cout << x << endl;
 
+    // copy string
+    string s1 = "hello";
+    string s2(s1, 2);
+    string s3 = s1 + "  not hello";
+    string s4;
+    s4 = s3;
+    s4.erase(7, s4.length() - 1); // 从第7个地方开始erase（包括第七个地方）
+    cout<<s1<<endl; // hello
+    cout<<s2<<endl; // llo
+    cout<<s3<<endl; // hello not hello
+    cout<<s4<<endl; //
     return 0;
 }
