deac2852c306712c02ba651e3d9a41de4443502a
David Gao <582435572@qq.com>
2021-10-28 12:58:27 +0800

    Update buildTree.cpp

--- buildTree.cpp
+++ buildTree.cpp
@@ -46,6 +46,7 @@ using namespace std;
 #include <iostream>
 #include "vector"
 #include <stack>
+#include "unordered_map"
 
 struct TreeNode {
     int val;
@@ -56,39 +57,73 @@ struct TreeNode {
 };
 
 class Solution {
+private:
+    unordered_map<int, int> index; // 通过map快速定位根结点
+
 public:
-    TreeNode* buildTree(vector<int> &preorder, vector<int> &inorder) {
+    TreeNode *
+    myBuildTree(vector<int> &preorder, vector<int> &inorder, int preorderLeft, int preorderRight, int inorderLeft,
+                int inorderRight) {
+
+        if (preorderLeft > preorderRight) return nullptr;
+
         // preorder: N L R [中 左 右]
         // inorder: L N R [左 中 右]
         // postorder: L R N [左 右 中]
 
-        // 1. get the root node from preorder(中 左 右)
-        TreeNode *root = new TreeNode(preorder[0]);
-        // 2. split node into [left root right] parts (inorder)
-        vector<int> left, right;
-        bool flag = true;
-        for (int i = 0; i < inorder.size(); i++) {
-            if (inorder[i] != preorder[0] && flag) { // left
-                left.push_back(inorder[i]);
-            }
-            if (inorder[i] == preorder[0]) {
-                flag = false;
-            }
-            if (inorder[i] != preorder[0] && !flag) { // right
-                right.push_back(inorder[i]);
-            }
-        }
+        // preorder: root, left, right, the 1st node will be the root node
+        int preorderRoot = preorderLeft;
+
+        // find the root node in inorder list
+        int inorderRoot = index[preorder[preorderRoot]]; // inorderRoot 的左边都是left sub tree
+
+        // create root
+        TreeNode *root = new TreeNode(preorder[preorderRoot]);
+
+        // get left subtree size
+        int leftSubTreeSize = inorderRoot - inorderLeft;
+
+        // recursively create leftSub, then connect to root
+        root->left = myBuildTree(
+                preorder,
+                inorder,
+                preorderLeft + 1,
+                preorderLeft + leftSubTreeSize,
+                inorderLeft,
+                inorderRoot - 1
+        );
+
+        // recursively create rightSub, then connect to root
+        root->right = myBuildTree(
+                preorder,
+                inorder,
+                preorderLeft + leftSubTreeSize + 1,
+                preorderRight,
+                inorderRoot + 1,
+                inorderRight
+        );
 
         return root;
     }
 
-    void postOrderPrintTree(TreeNode* root){
-        std::stack<TreeNode*> stack;
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+        int preorderSize = preorder.size();
+
+        // map the index of inorder to find root node easily
+        for (int i = 0; i < preorderSize; i++) {
+            index[inorder[i]] = i;
+        }
+
+        return myBuildTree(preorder, inorder, 0, preorderSize - 1, 0, preorderSize - 1);
+    }
+
+    void postOrderPrintTree(TreeNode *root) {
+        std::stack<TreeNode *> stack;
         std::vector<int> result;
         if (root == nullptr) return;
         stack.push(root);
         while (!stack.empty()) {
-            TreeNode* node = stack.top();
+            TreeNode *node = stack.top();
             stack.pop();
             result.push_back(node->val);
             if (node->left) stack.push(node->left);
@@ -106,11 +141,12 @@ public:
         std::cout << ']';
     }
 };
+
 //leetcode submit region end(Prohibit modification and deletion)
 int main() {
-    vector<int>preorder = {3,9,20,15,7};
-    vector<int>inorder = {9,3,15,20,7};
-    Solution* solurtion = new Solution();
-    TreeNode* answer = solurtion->buildTree(preorder, inorder);
+    vector<int> preorder = {3, 9, 20, 15, 7};
+    vector<int> inorder = {9, 3, 15, 20, 7};
+    Solution *solurtion = new Solution();
+    TreeNode *answer = solurtion->buildTree(preorder, inorder);
     solurtion->postOrderPrintTree(answer);
 };
