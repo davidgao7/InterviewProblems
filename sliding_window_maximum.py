fa909c8382a2839c5a4d28522b1966c1a0dfe4b3
David Gao <gao.ten@northeastern.edu>
2023-11-15 16:10:24 -0500

    LC239 sliding window maximum <deque>

--- sliding_window_maximum.py
+++ sliding_window_maximum.py
@@ -45,23 +45,42 @@ from typing import List
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
-
-    class MyHeap(object):
-        def __init__(self, ):
-
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        import heapq  # creating priority queue
+        import collections
 
-        #  building the max heap
-        # time complexity: O(number of elements)
-        for i in range(0, len(nums)):
-            if i==0:
-                heapq.heapify(nums[i])
-            else:
-                heapq.heappush(nums[i])
+        output = []
+        q = collections.deque()  # index
+        l = r = 0  # window
 
-        pass
+        while r < len(nums):
+            # make sure no smaller in the queue
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()  # pop the elem smaller than current e
 
+            # add new value
+            q.append(r)
+
+            # if left index out of bound, remove left val from window
+            if l > q[0]:
+                q.popleft()  # shift the window to right
+
+            if (r+1) >=k:
+                output.append(nums[q[0]])
+                l += 1
+
+            r += 1
+        return output
+
+
+
+# Window position                Max
+# ---------------               -----
+# [1  3  -1] -3  5  3  6  7       3
+#  1 [3  -1  -3] 5  3  6  7       3
+#  1  3 [-1  -3  5] 3  6  7       5
+#  1  3  -1 [-3  5  3] 6  7       5
+#  1  3  -1  -3 [5  3  6] 7       6
+#  1  3  -1  -3  5 [3  6  7]      7
 
 # leetcode submit region end(Prohibit modification and deletion)
 
