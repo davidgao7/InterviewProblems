049fc1700634ffe971e014cf61656f4f008773fe
David Gao <gao.ten@northeastern.edu>
2023-12-16 16:39:14 -0500

    tree postorder

---
+++ 145.postorderTraversal.py
@@ -0,0 +1,71 @@
+# Given the root of a binary tree, return the postorder traversal of its nodes'
+# values.
+#
+#
+#  Example 1:
+#
+#
+# Input: root = [1,null,2,3]
+# Output: [3,2,1]
+#
+#
+#  Example 2:
+#
+#
+# Input: root = []
+# Output: []
+#
+#
+#  Example 3:
+#
+#
+# Input: root = [1]
+# Output: [1]
+#
+#
+#
+#  Constraints:
+#
+#
+#  The number of the nodes in the tree is in the range [0, 100].
+#  -100 <= Node.val <= 100
+#
+#
+#
+# Follow up: Recursive solution is trivial, could you do it iteratively?
+#
+#  Related Topics Stack Tree Depth-First Search Binary Tree 👍 6610 👎 183
+from typing import Optional, List
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        return (
+            self.postorderTraversal(root.left)
+            + self.postorderTraversal(root.right)
+            + [root.val]
+        )
+
+# leetcode submit region end(Prohibit modification and deletion)
+# inorder:  L N R
+# preorder: N L R
+# postorder: L R N
+if __name__ == '__main__':
+    s = Solution()
+    result = [3,2,1]
+    root = TreeNode(
+        1,TreeNode(2, TreeNode(3), None)
+    )
+    print(s.postorderTraversal(root))
+    print(result)
