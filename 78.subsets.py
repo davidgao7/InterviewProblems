8988083d69b7a07577ddf979c92315196e38ca58
David Gao <gao.ten@northeastern.edu>
2024-01-08 20:22:21 -0500

    78.subsets of a list, backtracking to find path in tree

---
+++ 78.subsets.py
@@ -0,0 +1,56 @@
+# Given an integer array nums of unique elements, return all possible subsets (
+# the power set).
+#
+#  The solution set must not contain duplicate subsets. Return the solution in
+# any order.
+#
+#
+#  Example 1:
+#
+#
+# Input: nums = [1,2,3]
+# Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+#
+#
+#  Example 2:
+#
+#
+# Input: nums = [0]
+# Output: [[],[0]]
+#
+#
+#
+#  Constraints:
+#
+#
+#  1 <= nums.length <= 10
+#  -10 <= nums[i] <= 10
+#  All the numbers of nums are unique.
+#
+#
+#  Related Topics Array Backtracking Bit Manipulation 👍 16353 👎 248
+from typing import List
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        path = []
+        result = []
+        self.backtracking(nums, 0, path, result)
+        return result
+
+    def backtracking(self, nums: List[int], start_index: int, path, result) -> List[List[int]]:
+        # store the current path in current recursive
+        result.append(path[:])
+
+        if start_index >= len(nums):  # the loop won't execute if this satisfies anyway , no need to add
+            return
+
+        for i in range(start_index, len(nums)):
+            path.append(nums[i])
+            self.backtracking(nums, i + 1, path, result)
+            path.pop()
+
+
+# leetcode submit region end(Prohibit modification and deletion)
