abd3cd0b6d57a832f5596954b4edadf07823633c
David Gao <gao.ten@northeastern.edu>
2024-01-12 15:42:55 -0500

    47.need to level check(for each recursive call) if nubmer is same as pervious one, also check is previous has been used or not

---
+++ 47.PermutationII.py
@@ -0,0 +1,71 @@
+# Given a collection of numbers, nums, that might contain duplicates, return
+# all possible unique permutations in any order.
+#
+#
+#  Example 1:
+#
+#
+# Input: nums = [1,1,2]
+# Output:
+# [[1,1,2],
+#  [1,2,1],
+#  [2,1,1]]
+#
+#
+#  Example 2:
+#
+#
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+#
+#
+#
+#  Constraints:
+#
+#
+#  1 <= nums.length <= 8
+#  -10 <= nums[i] <= 10
+#
+#
+#  Related Topics Array Backtracking 👍 8291 👎 138
+from typing import List
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        used = [False] * len(nums)
+        self.backtracking(nums, used, [], result)
+        return result
+
+    def backtracking(
+        self, nums: List[int], used: List[int], path: List[int], result: List[List[int]]
+    ):
+
+        # reach the leaf
+        if len(path) == len(nums):
+            result.append(path[:])
+            return
+
+        for i in range(0, len(nums)):
+
+            if i > 0 and nums[i] == nums[i - 1] and used[i - 1] == False :
+                # level check if number is used
+                continue  # go to another branch
+
+            if used[i]:
+                # cannot choose this number
+                continue
+
+            # we can choose this number
+            used[i] = True
+            # collect element to from path
+            path.append(nums[i])
+            # choose another element
+            self.backtracking(nums, used, path, result)
+            used[i] = False
+            path.pop(-1)
+
+
+# leetcode submit region end(Prohibit modification and deletion)
