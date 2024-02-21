404fd761283afbc0a3a3bb7e2a276461a1bf1828
David Gao <gao.ten@northeastern.edu>
2024-02-21 12:15:33 -0500

    125.isPalindrome.py two pointer
    ascii use ord()

---
+++ 125.isPalindrome.py
@@ -0,0 +1,46 @@
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+# Example 2:
+#
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+# Example 3:
+#
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+
+
+class Solution:
+
+    def isPalindrome(self, s: str) -> bool:
+        # two pointers
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            # compare
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+
+    def alphaNum(self, c):
+        # check if c is alphanumeric
+        return (
+            ord("A") <= ord(c) <= ord("Z")
+            or ord("a") <= ord(c) <= ord("z")
+            or ord("0") <= ord(c) <= ord("9")
+        )
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.isPalindrome("A man, a plan, a canal: Panama"))
