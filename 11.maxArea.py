4edb942890b2efdbc004b451cd72f86213694d16
David Gao <gao.ten@northeastern.edu>
2024-02-23 12:10:25 -0500

    11.maxArea.py, two pointers

---
+++ 11.maxArea.py
@@ -0,0 +1,56 @@
+# You are given an integer array height of length n. There are n vertical lines
+# drawn such that the two endpoints of the iᵗʰ line are (i, 0) and (i, height[i]).
+#
+#
+#  Find two lines that together with the x-axis form a container, such that the
+# container contains the most water.
+#
+#  Return the maximum amount of water a container can store.
+#
+#  Notice that you may not slant the container.
+#
+#
+#  Example 1:
+#
+#
+# Input: height = [1,8,6,2,5,4,8,3,7]
+# Output: 49
+# Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,
+# 3,7]. In this case, the max area of water (blue section) the container can
+# contain is 49.
+#
+#
+#  Example 2:
+#
+#
+# Input: height = [1,1]
+# Output: 1
+#
+#
+#
+#  Constraints:
+#
+#
+#  n == height.length
+#  2 <= n <= 10⁵
+#  0 <= height[i] <= 10⁴
+#
+#
+#  Related Topics Array Two Pointers Greedy 👍 28152 👎 1640
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        result = 0
+        l, r = 0, len(height) - 1
+        while l < r:
+            result = max(result, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return result
+
+
+# leetcode submit region end(Prohibit modification and deletion)
