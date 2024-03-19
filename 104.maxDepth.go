daa54f6d00d14d80e32e97d8bc882363cb40edbb
David Gao <gao.ten@northeastern.edu>
2024-03-19 18:21:07 -0400

    104.tree maxDepth go

---
+++ 104.maxDepth.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package interviewproblems
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
