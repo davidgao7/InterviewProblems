9d883ee8b8534a05042a1991b73936a8ace44038
David Gao <gao.ten@northeastern.edu>
2024-03-20 21:47:31 -0400

    104.maxDepth add go test

--- 104.maxDepth.go
+++ 104.maxDepth.go
@@ -6,7 +6,9 @@
  *     Right *TreeNode
  * }
  */
-package interviewproblems
+package main
+
+import "fmt"
 
 type TreeNode struct {
 	Val   int
@@ -14,9 +16,38 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
 	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
+
+func main() {
+
+	root := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+
+	fmt.Println(maxDepth(root))
+
 }
