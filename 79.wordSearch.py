c879512383f3aecb7794bd93674c85f6f6f7c15c
David Gao <gao.ten@northeastern.edu>
2024-04-08 09:49:11 -0400

    79.wordsearch, backtracking
    movement like a snake game

---
+++ 79.wordSearch.py
@@ -0,0 +1,74 @@
+# Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+#
+# The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+# Output: true
+# Example 2:
+#
+#
+# Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+# Output: true
+# Example 3:
+#
+#
+# Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+# Output: false
+#
+#
+# Constraints:
+#
+# m == board.length
+# n = board[i].length
+# 1 <= m, n <= 6
+# 1 <= word.length <= 15
+# board and word consists of only lowercase and uppercase English letters.
+#
+#
+# Follow up: Could you use search pruning to make your solution faster with a larger board?
+
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        """
+        use backtracking to find the word in the board
+        """
+
+        ROWS, COLS = len(board), len(board[0])
+        path = set()
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (
+                r < 0
+                or c < 0
+                or r >= ROWS
+                or c >= COLS
+                or word[i] != board[r][c]
+                or (r, c) in path
+            ):
+                return False
+
+            path.add((r, c))
+            res = (
+                dfs(r + 1, c, i + 1)
+                or dfs(r - 1, c, i + 1)
+                or dfs(r, c + 1, i + 1)
+                or dfs(r, c - 1, i + 1)
+            )
+            path.remove((r, c))
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):  # start at the beginning of the word
+                    return True
+
+
+# O(n * m * dfs) 4^len(word)
