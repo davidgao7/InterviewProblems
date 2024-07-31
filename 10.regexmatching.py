60e234e39a1a5e5af5f8c5df54ac0c43db20f8fb
David Gao <gao.ten@northeastern.edu>
2024-07-31 15:52:22 -0400

    bottom up dp

--- 10.regexmatching.py
+++ 10.regexmatching.py
@@ -86,3 +86,25 @@ class Solution:
             return False
 
         return dfs(0, 0)
+
+    def isMatch2(self, s: str, p: str) -> bool:
+        """
+        s: input string
+        p: pattern
+        """
+
+        # bottom up dp
+        m, n = len(s), len(p)
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = True
+        for i in range(2, n + 1):
+            dp[0][i] = dp[0][i - 2] and p[i - 1] == "*"
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+            if p[j - 1] == "*":
+                dp[i][j] = dp[i][j -
+                                 2] or (s[i - 1] == p[j - 2] or p[j - 2] == ".") and dp[i - 1][j]
+            else:
+                dp[i][j] = dp[i - 1][j -
+                                     1] and (s[i - 1] == p[j - 1] or p[j - 1] == ".")
+        return dp[m][n]
