bba1ea50445112eeb7d25baea561e4afc41098b3
David Gao <gao.ten@northeastern.edu>
2024-07-31 16:03:16 -0400

    format

--- 10.regexmatching.py
+++ 10.regexmatching.py
@@ -101,10 +101,14 @@ class Solution:
             dp[0][i] = dp[0][i - 2] and p[i - 1] == "*"
         for i in range(1, m + 1):
             for j in range(1, n + 1):
-            if p[j - 1] == "*":
-                dp[i][j] = dp[i][j -
-                                 2] or (s[i - 1] == p[j - 2] or p[j - 2] == ".") and dp[i - 1][j]
-            else:
-                dp[i][j] = dp[i - 1][j -
-                                     1] and (s[i - 1] == p[j - 1] or p[j - 1] == ".")
+                if p[j - 1] == "*":
+                    dp[i][j] = (
+                        dp[i][j - 2]
+                        or (s[i - 1] == p[j - 2] or p[j - 2] == ".")
+                        and dp[i - 1][j]
+                    )
+                else:
+                    dp[i][j] = dp[i - 1][j - 1] and (
+                        s[i - 1] == p[j - 1] or p[j - 1] == "."
+                    )
         return dp[m][n]
