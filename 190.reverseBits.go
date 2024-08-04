ca3674f396cf4a57c7ef052f3a9efefecca85d22
David Gao <gao.ten@northeastern.edu>
2024-08-04 09:19:10 -0400

    190.reverseBits.go go bit operation example

---
+++ 190.reverseBits.go
@@ -0,0 +1,14 @@
+package main
+
+func reverseBits(num uint32) uint32 {
+	var res uint32 = 0
+
+	for i := 0; i < 32; i++ {
+		// getting the nth bit of num
+		bit := (num >> i) & 1
+		// put into the 31-i th position of the res
+		res = res | (bit << (31 - i))
+	}
+
+	return res
+}
