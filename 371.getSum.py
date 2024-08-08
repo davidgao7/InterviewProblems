d27d3b966fcd295ab86da5c2f2243ca2dc444183
David Gao <gao.ten@northeastern.edu>
2024-08-08 15:47:03 -0400

    371.getsum, bit

---
+++ 371.getSum.py
@@ -0,0 +1,44 @@
+"""
+371. Sum of Two Integers
+Solved
+Medium
+Topics
+Companies
+Given two integers a and b, return the sum of the two integers without using
+the operators + and -.
+
+
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = 2, b = 3
+Output: 5
+
+
+Constraints:
+
+-1000 <= a, b <= 1000
+Seen this question in a real interview before?
+"""
+
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        def add(a, b):
+            if not a or not b:
+                return a or b
+            return add(a ^ b, (a & b) << 1)
+
+        if a * b < 0:  # assume a < 0, b > 0
+            if a > 0:
+                return self.getSum(b, a)
+            if add(~a, 1) == b:  # -a == b
+                return 0
+            if add(~a, 1) < b:  # -a < b
+                return add(~add(add(~a, 1), add(~b, 1)), 1)  # -add(-a, -b)
+
+        return add(a, b)  # a*b >= 0 or (-a) > b > 0
